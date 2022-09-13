d1d4ec1e831cb8ab166ca8efc7e9c32bcf19f680
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2022-09-13 07:56:34 -0400

    get specified number of people using lazy loading

--- lib/src/test/java/rxjava/examples/PersonBLTest.java
+++ lib/src/test/java/rxjava/examples/PersonBLTest.java
@@ -1,13 +1,25 @@
 package rxjava.examples;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 public class PersonBLTest {
-    PersonBL personBL = new PersonBL(new PersonDao());
+
+    @Spy
+    PersonDao personDao;
+
+    @InjectMocks
+    PersonBL personBL;
 
 
     @Test
@@ -22,6 +34,9 @@ public class PersonBLTest {
         List<Person> specifiedNumberOfPeople = personBL.getSpecifiedNumberOfPeople(3);
 
         assertEquals(3, specifiedNumberOfPeople.size());
+        ArgumentCaptor<Integer> pageCaptor = ArgumentCaptor.forClass(Integer.class);
+        Mockito.verify(personDao, Mockito.times(1)).listPeople(pageCaptor.capture());
+        assertEquals(1, pageCaptor.getAllValues().size());
     }
 
     @Test
@@ -29,5 +44,8 @@ public class PersonBLTest {
         List<Person> specifiedNumberOfPeople = personBL.getSpecifiedNumberOfPeople(25);
 
         assertEquals(25, specifiedNumberOfPeople.size());
+        ArgumentCaptor<Integer> pageCaptor = ArgumentCaptor.forClass(Integer.class);
+        Mockito.verify(personDao, Mockito.times(3)).listPeople(pageCaptor.capture());
+        assertEquals(3, pageCaptor.getAllValues().size());
     }
 }
