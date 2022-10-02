c890b36d5fb922afcc5785c9491169ae3ee318f4
Vijay <vijay.k.vepakomma@gmail.com>
2022-10-02 15:12:45 -0400

    rxnetty tcp server

---
+++ lib/src/main/java/rxjava/examples/httpservers/netty/EurUsdCurrencyTcpServer.java
@@ -0,0 +1,35 @@
+package rxjava.examples.httpservers.netty;
+
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.reactivex.netty.protocol.tcp.server.TcpServer;
+import rx.Observable;
+
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+
+public class EurUsdCurrencyTcpServer {
+    private static final BigDecimal RATE = new BigDecimal("1.06448");
+
+    public static void main(String[] args) {
+        TcpServer.newServer(8080)
+                .<String, String>pipelineConfigurator(pipeline -> {
+                    pipeline.addLast(new LineBasedFrameDecoder(1024));
+                    pipeline.addLast(new StringDecoder(StandardCharsets.UTF_8));
+                })
+                .start(connection -> {
+                    Observable<String> output = connection.getInput()
+                            .map(BigDecimal::new)
+                            .flatMap(EurUsdCurrencyTcpServer::eurToUsd);
+
+                    return connection.writeAndFlushOnEach(output);
+                })
+                .awaitShutdown();
+    }
+
+    private static Observable<String> eurToUsd(BigDecimal eur) {
+        return Observable.just(eur.multiply(RATE)).map(amount -> eur + "EUR is " + amount + " USD\r\n")
+                .delay(1, TimeUnit.SECONDS);
+    }
+}
